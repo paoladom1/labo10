9f1c5c64dead55f360d5a92f5bb46ced4265dc7c
Paola Dominguez <paoladomdiaz@gmail.com>
2019-11-07 16:31:33 -0600

    add: update y deleteOne methods

--- contollers/RegisterController.js
+++ contollers/RegisterController.js
@@ -40,8 +40,36 @@ const getOneRegister = (req, res) => {
   });
 };
 
+const deleteOneRegister = (req, res) => {
+  Register.deleteOne({ _id: req.params.id }, error => {
+    if (error)
+      return res.status(500).json({
+        msg: "error"
+      });
+
+    return res.status(200).json({
+      msg: "ok"
+    });
+  });
+};
+
+const updateRegister = (req, res) => {
+  Register.updateOne(req.body.filter, req.body.update, (error, doc) => {
+    if (error)
+      return res.status(500).json({
+        msg: "hubo un error"
+      });
+    return res.status(200).json({
+      msg: "ok",
+      registers: doc
+    });
+  })
+}
+
 module.exports = {
   getRegister,
   insert,
-  getOneRegister
+  getOneRegister,
+  deleteOneRegister,
+  updateRegister
 };
--- routes/register.js
+++ routes/register.js
@@ -5,6 +5,9 @@ const registerController = require("./../contollers/RegisterController");
 router.get("/", registerController.getRegister);
 router.get("/:id", registerController.getOneRegister);
 router.post("/", registerController.insert);
+router.delete("/:id", registerController.deleteOneRegister);
+router.put("/", registerController.updateRegister);
+router.patch("/", registerController.updateRegister);
 
 
 module.exports = router;
